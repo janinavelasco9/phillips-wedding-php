bd81e4d5a3922a058e016970832c20a3875ac04e
Janina Velasco <jvelasco0226@gmail.com>
2016-10-30 13:06:52 -0400

    maybeee its gonna work

--- web/rsvp.php
+++ web/rsvp.php
@@ -1,8 +1,8 @@
 <?php
 if(!isset($_POST['submit']))
 {
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
+    //This page should not be accessed directly. Need to submit the form.
+    echo "error; you need to submit the form!";
 }
 $first_name = $_POST['firstName'];
 $last_name = $_POST['lastName'];
@@ -26,16 +26,16 @@ $email_from = 'jvelasco0226@gmail.com';//<== update the email address
 $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $first_name $last_name.\n".
     "Attending:\n $attending".
-	"Here is the message:\n $message".
+    "Here is the message:\n $message".
 
 $to = "jvelasco0226@gmail.com";//<== update the email address
 $headers = "From: $first_name $last_name \r\n";
 // $headers .= "Reply-To: $visitor_email \r\n";
+
 //Send the email!
-mail($to,$email_subject,$email_body,$headers);
+mail("jvelasco0226@gmail.com",$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
 header('Location: thank-you');
-
 
 // Function to validate against any email injection attempts
 function IsInjected($str)
