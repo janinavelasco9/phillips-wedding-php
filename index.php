e2307a7e0401261ea5f56e022f44793c79d89e21
Janina Velasco <jvelasco0226@gmail.com>
2016-10-30 12:43:16 -0400

    trying something

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require('../vendor/autoload.php');
+require('vendor/autoload.php');
 
 $app = new Silex\Application();
 $app['debug'] = true;
@@ -12,7 +12,7 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+    'twig.path' => __DIR__.'/web/views',
 ));
 
 // Our web handlers
@@ -20,6 +20,31 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
+});
+
+$app->get('/our-wedding', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('our-wedding.twig');
+});
+
+$app->get('/details', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('details.twig');
+});
+
+$app->get('/photographs', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('photographs.twig');
+});
+
+$app->get('/rsvp', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('rsvp.twig');
+});
+
+$app->get('/thank-you', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('thank-you.twig');
 });
 
 $app->run();
