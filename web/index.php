22f932d557e5a5811337c0b389582c8c2072bf07
Janina Velasco <jvelasco0226@gmail.com>
2016-10-30 11:59:57 -0400

    adding the rest of the pages

--- web/index.php
+++ web/index.php
@@ -27,4 +27,24 @@ $app->get('/our-wedding', function() use($app) {
   return $app['twig']->render('our-wedding.twig');
 });
 
+$app->get('/details', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('details.twig');
+});
+
+$app->get('/photographs', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('photographs.twig');
+});
+
+$app->get('/rsvp', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('rsvp.twig');
+});
+
+$app->get('/thank-you', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('thank-you.twig');
+});
+
 $app->run();
